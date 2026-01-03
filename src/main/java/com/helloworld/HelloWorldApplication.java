dd7a67a1975cbf2a99ff14a47505123626c06245
onizukaTP <tharunprabu2418@gmail.com>
2026-01-03 21:19:57 +0530

    UC5: added logger to log messages

--- src/main/java/com/helloworld/HelloWorldApplication.java
+++ src/main/java/com/helloworld/HelloWorldApplication.java
@@ -1,14 +1,23 @@
 package com.helloworld;
 
+import com.helloworld.component.DemoBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class HelloWorldApplication {
 
+    public static final Logger logger = LoggerFactory.getLogger(HelloWorldApplication.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        SpringApplication.run(HelloWorldApplication.class, args);
+
+        logger.debug("Welcome to spring concept demo");
+        ApplicationContext context = SpringApplication.run(HelloWorldApplication.class, args);
+        logger.debug("Checking Context: {}", context.getBean(DemoBean.class));
+        logger.debug("\n*** Example using @Autowire annotation on property ***");
     }
 
 }
