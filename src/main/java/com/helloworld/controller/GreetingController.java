124fc603cecbb835578ec33b68dd675b2ad43a59
onizukaTP <tharunprabu2418@gmail.com>
2026-01-04 17:37:45 +0530

    [tp][messaging-app][day3] UC2: extend GreetingController to use services layer to get a simple greeting

--- src/main/java/com/helloworld/controller/GreetingController.java
+++ src/main/java/com/helloworld/controller/GreetingController.java
@@ -1,19 +1,33 @@
 package com.helloworld.controller;
 
 import com.helloworld.model.Greeting;
+import com.helloworld.model.User;
+import com.helloworld.service.GreetingService;
+import com.helloworld.service.IGreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@RequestMapping("/greetings")
 public class GreetingController {
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/greeting")
-    public Greeting greeting (@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    @Autowired
+    private GreetingService greetingService;
+
+    @GetMapping("/hello")
+    public Greeting sayHello() {
+        return greetingService.sayHello();
     }
+
+//    @GetMapping("")
+//    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+//        User user = new User();
+//        user.setFirstName(name);
+//        return greetingService.addGreeting(user);
+//    }
 }
---
+++ src/main/java/com/helloworld/service/GreetingService.java
@@ -0,0 +1,19 @@
+package com.helloworld.service;
+
+import com.helloworld.model.Greeting;
+import com.helloworld.model.User;
+import com.helloworld.repository.GreetingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class GreetingService {
+    private static final String template = "Hello, %s";
+    private final AtomicLong counter = new AtomicLong();
+
+    public Greeting sayHello() {
+        return new Greeting(counter.incrementAndGet(), "Hello World");
+    }
+}
